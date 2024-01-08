7c12e9e76208646f0961d960ac9a2246a1a970c5
Manfred Dreese <manfred.dreese@codecentric.de>
2024-01-08 11:35:19 +0100

    add: output minutes status to actual LEDs on board

---
+++ src/hal.h
@@ -0,0 +1,6 @@
+#include "Arduino.h"
+
+#define GPIO_MINUTE_BIT_0 D3
+#define GPIO_MINUTE_BIT_1 D4
+#define GPIO_MINUTE_BIT_2 D5
+#define GPIO_MINUTE_BIT_3 D6
--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
  * then off for one second, repeatedly.
  */
 #include "Arduino.h"
+#include <stdio.h>
 #include <clock_conversion.h>
+#include "hal.h"
 
 #ifndef LED_BUILTIN
 #define LED_BUILTIN 2
@@ -13,21 +15,36 @@
 
 void setup()
 {
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+    // initialize LED digital pin as an output.
+    pinMode(GPIO_MINUTE_BIT_0, OUTPUT);
+    pinMode(GPIO_MINUTE_BIT_1, OUTPUT);
+    pinMode(GPIO_MINUTE_BIT_2, OUTPUT);
+    pinMode(GPIO_MINUTE_BIT_3, OUTPUT);
 }
 
 void loop()
 {
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+    const int32_t fakeTimestamp = 1704540720;
+    const int32_t timestampMask = convertTimestampToMinuteBits(fakeTimestamp);
 
-  // wait for a second
-  delay(100);
+    // turn the LED on (HIGH is the voltage level)
+    digitalWrite(GPIO_MINUTE_BIT_0, timestampMask & 0b1000);
+    delay(30);
+    digitalWrite(GPIO_MINUTE_BIT_1, timestampMask & 0b0100);
+    delay(30);
+    digitalWrite(GPIO_MINUTE_BIT_2, timestampMask & 0b0010);
+    delay(30);
+    digitalWrite(GPIO_MINUTE_BIT_3, timestampMask & 0b0001);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+    // wait for a second
+    delay(100);
 
-   // wait for a second
-  delay(100);
+    // turn the LED off by making the voltage LOW
+    digitalWrite(GPIO_MINUTE_BIT_0, LOW);    
+    digitalWrite(GPIO_MINUTE_BIT_1, LOW);
+    digitalWrite(GPIO_MINUTE_BIT_2, LOW);    
+    digitalWrite(GPIO_MINUTE_BIT_3, LOW);
+
+    // wait for a second
+    delay(100);
 }
